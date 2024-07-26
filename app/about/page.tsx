3ee7a771fb1df84f27d5c21a23c164d78c5184ac
Benn Eichhorn <benn@eichhorn.co>
2024-07-26 20:38:46 +1000

    Added About page

---
+++ app/about/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import {
+    Link,
+  } from "@nextui-org/react";
+
+export default function About() {
+
+  return (
+    <div className="w-screen h-screen flex flex-col">
+      <div className="w-[900px] flex flex-col items-start justify-start gap-5 mx-auto pt-4 pb-20">
+        <div className="w-full">
+          The about page
+        </div>
+      </div>
+    </div>
+  );
+}
