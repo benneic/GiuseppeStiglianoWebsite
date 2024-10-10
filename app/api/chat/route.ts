15cb1bc6e970ad2e52388cc8802f4c9118358187
Benn Eichhorn <benn@eichhorn.co>
2024-10-10 22:07:40 +1000

    change OpenAI model

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,4 @@
-//import { openai } from "@ai-sdk/openai";
-import { createOpenAI } from "@ai-sdk/openai";
+import { openai } from "@ai-sdk/openai";
 import { streamText } from "ai";
 
 // Allow streaming responses up to 30 seconds
@@ -8,14 +7,8 @@ export const maxDuration = 30;
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
-  const openai = createOpenAI({
-    // https://sdk.vercel.ai/providers/ai-sdk-providers/openai
-    // custom settings, e.g.
-    compatibility: 'strict', // strict mode, enable when using the OpenAI API
-  });
-
   const result = await streamText({
-    model: openai("gpt-4o-mini"),
+    model: openai("gpt-4-turbo"),
     messages,
   });
 
